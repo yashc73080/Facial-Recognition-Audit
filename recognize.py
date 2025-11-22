66e7b5f17b6e78b10a1ace97a67e21c8a3734f08
Yash Chennawar <yash.chennawar@gmail.com>
2025-11-22 15:39:29 -0500

    Everypixel recognition

---
+++ recognize.py
@@ -0,0 +1,31 @@
+import requests
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv('.env')
+
+client_id = os.getenv('CLIENT_KEY')
+client_secret = os.getenv('SECRET_KEY')
+
+image_path = 'images/face13.jpg'
+
+with open(image_path, 'rb') as f:
+    files = {'data': f}
+    keywords = requests.post(
+        'https://api.everypixel.com/v1/keywords',
+        files=files,
+        auth=(client_id, client_secret)
+    ).json()
+
+# Re-open file for second endpoint (stream was consumed)
+with open(image_path, 'rb') as f:
+    files = {'data': f}
+    faces = requests.post(
+        'https://api.everypixel.com/v1/faces',
+        files=files,
+        auth=(client_id, client_secret)
+    ).json()
+
+print(json.dumps(keywords, indent=4))
+print(json.dumps(faces, indent=4))
